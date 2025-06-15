9a34c431099849f3eb29a124c989fbb3a966e80b
soniccc <20264635+soniccc@users.noreply.github.com>
2025-06-15 18:06:23 +0100

    Add simple hello world Python script

---
+++ hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Simple Hello World Python Script
+"""
+
+def main():
+    print("Hello, World!")
+    print("Welcome to the Docker MCP test repository!")
+
+if __name__ == "__main__":
+    main()
